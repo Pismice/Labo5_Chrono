adff8647906219e9a4365d66d167ba73ee6c580e
Pismice <33352709+Pismice@users.noreply.github.com>
2021-11-10 12:02:48 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Labo5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo5.dir/main.cpp.obj"
+  "Labo5.exe"
+  "Labo5.exe.manifest"
+  "Labo5.pdb"
+  "libLabo5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : main.cpp (xxx = h ou cpp)
+Auteur(s)       : Jérémie Santoro, Didier Lokokpe
+Date creation   : 10.11.2021
+
+Description     : Ce programme permet de tester la dexterité et la rapidité au
+                  clavier de l'utilisteur. En fonction du nombre de test désirés
+                  par l'utilisateur [1,10] teste l'utilisateur avec différents
+                  charactères générés aléatoirement, l'utilisateur doit donc
+                  reproduire au clavier le charactère généré pour obtenir
+                  une réponse correcte. Le tout est chronométré ce qui permet
+                  de retourner à la fin du programme en plus du nombre de
+                  réponses correctes: le temps écoulé, ainsi que le temps
+                  moyen passé par lettre.
+                  L'utilisateur est ensuite invité à recommencer le programme.
+
+Remarque(s)     : - Les saisies utilisteur sont contrôlées
+
+Compilateur     : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ return EXIT_SUCCESS;
+}
