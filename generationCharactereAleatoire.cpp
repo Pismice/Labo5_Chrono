152138204cbb045260fe758275af00dd6daa8512
pismice <pismice15@gmail.com>
2021-11-12 17:01:39 +0100

    Ajout du seed de breguet

--- generationCharactereAleatoire.cpp
+++ generationCharactereAleatoire.cpp
@@ -3,7 +3,16 @@
 //
 #include <random>          // Librairie qui permet la génération de valeurs aléatoires
 
+static bool premiereEntree = true;
+
 char genererCharactereAleatoireEntre(const unsigned char& begin, const unsigned char& end){
+   // Si c'est la première fois il faut générer un nouveau seed
+   if(premiereEntree)
+   {
+      srand((unsigned int)time(nullptr));
+      premiereEntree = false;
+   }
+
    int premierCharactere = int(begin);
    int dernierCharactere = int(end);
 
